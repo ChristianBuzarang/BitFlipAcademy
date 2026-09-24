35ea6ae2d59d52d18f10b6f3af2c2784c69106da
Christian Buzarang <christianbuzarang@gmail.com>
2026-09-24 17:56:28 +0800

    fix(error): fixing error upon updating from previous actions

--- obj/Debug/net8.0/BitFlipBlazor.AssemblyInfo.cs
+++ obj/Debug/net8.0/BitFlipBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BitFlipBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e31aca5b11121767ec052c6ff5cd293ee1beabd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49828fa0fa5916d8543d2d733bbc2050245135f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BitFlipBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BitFlipBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
